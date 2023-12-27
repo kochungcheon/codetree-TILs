a8a8fa02421e3ea04ee7c7dba17d9d466a52e446
kochungcheon <g2c1010@naver.com>
2023-12-27 13:57:17 +0900

    Title: 연속된 수들의 합, Time: 124ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 231227/연속된 수들의 합/sum-of-consecutive-numbers.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = 0;
+    
+        int s = 1;
+        int e = 2;
+        int tmp = s + e;
+        while (e >= s && n >= e) {
+            if (tmp == n) {
+                ans++;
+                e++;
+                tmp+=e;
+            }
+            else if (tmp > n) {
+                tmp-=s;
+                s++;
+            } 
+            else {
+                e++;
+                tmp+=e;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
+
+//
