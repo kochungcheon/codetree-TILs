81ee0bb99e26a4acb8d97bd7d9a3692e2f8ae8cd
kochungcheon <g2c1010@naver.com>
2024-04-10 14:15:09 +0900

    Title: 범위 내에 있는 점의 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240410/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int[] num = new int[1000001];
+        
+        for (int a : arr) {
+            num[a-1]++;
+        }
+
+        int[] prefix = new int[1000002];
+        for (int i=0; i<1000001; i++) {
+            prefix[i+1] = num[i] + prefix[i];
+        }
+
+        for (int i=0; i<Q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            System.out.println(prefix[e] - prefix[s-1]);
+        }    
+    }
+}
