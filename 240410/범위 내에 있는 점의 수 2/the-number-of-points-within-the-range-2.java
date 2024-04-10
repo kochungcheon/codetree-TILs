767877bb82653f3b0f44e71f291e7fa09937fcc5
kochungcheon <g2c1010@naver.com>
2024-04-10 14:18:53 +0900

    Title: 범위 내에 있는 점의 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240410/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
+++ 240410/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
@@ -15,12 +15,12 @@ public class Main {
         int[] num = new int[1000001];
         
         for (int a : arr) {
-            num[a-1]++;
+            num[a]++;
         }
 
         int[] prefix = new int[1000002];
-        for (int i=0; i<1000001; i++) {
-            prefix[i+1] = num[i] + prefix[i];
+        for (int i=1; i<1000001; i++) {
+            prefix[i] = num[i] + prefix[i-1];
         }
 
         for (int i=0; i<Q; i++) {
