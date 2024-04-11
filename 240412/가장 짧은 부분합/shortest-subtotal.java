0e96e039d6d6e27b2bb7f7594f228bc89c728f08
kochungcheon <g2c1010@naver.com>
2024-04-12 00:38:42 +0900

    Title: 가장 짧은 부분합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240412/가장 짧은 부분합/shortest-subtotal.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    
+        int n = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int ans = 100001;
+
+        int total = 0;
+
+        int s = 0;
+        int e = 0;
+        while (n > s) {
+            total += arr[s];
+            while (total >= S) {
+                ans = Math.min(s - e + 1, ans);
+                e++;
+                total -= arr[e];
+            }
+            s++;
+        }
+        System.out.println(ans);
+    }
+}
