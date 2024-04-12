9fabcfe87cf81b525f78ba4f8f38956a87729b75
kochungcheon <g2c1010@naver.com>
2024-04-13 01:11:40 +0900

    Title: 바구니 안의 사탕, Time: 304ms, Memory: 51MB, Status: Wrong Answer - CodeTree

--- 240413/바구니 안의 사탕/candy-in-the-basket.java
+++ 240413/바구니 안의 사탕/candy-in-the-basket.java
@@ -8,7 +8,7 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[1000001];
+        int[] arr = new int[4000001];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int candy = Integer.parseInt(st.nextToken());
@@ -16,18 +16,18 @@ public class Main {
             arr[w] += candy; 
         }
 
-        int[] prefix = new int[1000010];
-        for (int i = 0; i <= 1000000; i++) {
+        int[] prefix = new int[4000002];
+        for (int i = 0; i < 4000001; i++) {
             prefix[i + 1] = prefix[i] + arr[i];
         }
 
         int ans = 0;
 
-        int R = 2 * K;
+        int R = 2 * K + 1;
         int L = 0;
-        while (R < 1000002) { 
-            if (prefix[R + 1] - prefix[L] > ans) {
-                ans = prefix[R + 1] - prefix[L];
+        while (R < 4000001) { 
+            if (prefix[R] - prefix[L] > ans) {
+                ans = prefix[R] - prefix[L];
             }
             L++;
             R++;
