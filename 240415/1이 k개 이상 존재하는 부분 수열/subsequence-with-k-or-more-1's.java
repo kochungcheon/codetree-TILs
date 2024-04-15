5ee852f2ddfd5dd122d9c0057bbb33ca6bf709cc
kochungcheon <g2c1010@naver.com>
2024-04-15 18:12:22 +0900

    Title: 1이 k개 이상 존재하는 부분 수열, Time: 247ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240415/1이 k개 이상 존재하는 부분 수열/subsequence-with-k-or-more-1's.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] one = new int[n];
+        for (int i=0; i<n; i++) {
+            if (arr[i] == 1) {
+                one[i] = 1;
+            }
+        }
+        int[] prefix = new int[n + 1];
+        for (int i=0; i<n; i++) {
+            prefix[i+1] = prefix[i] + one[i];
+        }
+        // for (int i=0; i<=n; i++) {
+        //     System.out.print(prefix[i] + " ");
+        // }
+        // System.out.println();
+
+        int L = 0;
+        int R = 1;
+
+        int ans = Integer.MAX_VALUE;
+        while (R >= L && n > R) {
+            int nowK = prefix[R] - prefix[L];
+            if (nowK >= k) {
+                ans = Math.min(ans, R - L);
+            }
+            if (nowK < k) {
+                R++;
+            } else {
+                L++;
+            }
+        }
+
+        if (ans == Integer.MAX_VALUE) {
+            System.out.println(-1);
+        } else {
+            System.out.println(ans);
+        }
+
+    }
+}
