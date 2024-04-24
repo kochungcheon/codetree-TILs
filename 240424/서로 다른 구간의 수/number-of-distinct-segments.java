aba2500fdbb0e4d54bedc3f602da8047a29959e9
kochungcheon <g2c1010@naver.com>
2024-04-24 15:26:33 +0900

    Title: 서로 다른 구간의 수, Time: 1245ms, Memory: 58MB, Status: Passed - CodeTree

--- 240424/서로 다른 구간의 수/number-of-distinct-segments.java
+++ 240424/서로 다른 구간의 수/number-of-distinct-segments.java
@@ -1,43 +1,66 @@
 import java.util.*;
+import java.util.stream.Collectors;
 import java.io.*;
 
 public class Main {
+
+    static class Point {
+        int x, v, index;
+
+        public Point(int x, int v, int index) {
+            this.x = x;
+            this.v = v;
+            this.index = index;
+        }
+
+        public int getX() {
+            return this.x;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Point(x=%d, v=%d, index=%d)", x, v, index);
+        }
+    }
+
+    static List<Point> points = new ArrayList<>();
+
+    static Set<Integer> segs = new HashSet<>();
+
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        // 2배 해준다
-        int[] arr = new int[200010];
-
-        int start = Integer.MAX_VALUE;
-        int end = 0;
-
         StringTokenizer st;
         for (int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken()) * 2;
-            int e = Integer.parseInt(st.nextToken()) * 2 + 1;
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
 
-            start = Math.min(start, s);
-            end = Math.max(end, e);
-
-            arr[s]++;
-            arr[e]--;
+            points.add(new Point(s, 1, i));
+            points.add(new Point(e, -1, i));
         }
 
-        int[] prefix = new int[200010];
-        for (int i=0; i<200009; i++) {
-            prefix[i+1] = prefix[i] + arr[i+1];
-        }
+        points = points.stream().sorted(Comparator.comparing(Point::getX)).collect(Collectors.toList());
 
         int ans = 0;
-        for (int i=start-1; i<=end; i++) {
-            if (prefix[i] == 0 && prefix[i-1] != 0) {
-                ans++;
+        
+        for (int i=0; i<2*N; i++) {
+            int x = points.get(i).x;
+            int v = points.get(i).v;
+            int index = points.get(i).index;
+
+            if (v == +1) {
+                if (segs.size() == 0) {
+                    ans++;
+                }
+                segs.add(index);
+            }
+            else {
+                segs.remove(index);
             }
         }
         System.out.println(ans);
-
     }
 }
