4bcb71c3cf132190ac7c6ec420c20d445da01660
kochungcheon <g2c1010@naver.com>
2024-04-25 12:27:20 +0900

    Title: 최대 구간의 크기, Time: 1072ms, Memory: 39MB, Status: Passed - CodeTree

---
+++ 240425/최대 구간의 크기/size-of-largest-interval.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+import java.util.stream.*;
+public class Main {
+    static class Node {
+        int x; int v; int index;
+        public Node (int x, int v, int index) {
+            this.x = x;
+            this.v = v;
+            this.index = index;
+        }
+        public int getX() {
+            return this.x;
+        }
+    }
+    static List<Node> nodes = new ArrayList<>();
+    static Set<Integer> segs = new HashSet<>();
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        for (int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            nodes.add(new Node(a, 1, i));
+            nodes.add(new Node(b, -1, i));
+        }
+
+        nodes = nodes.stream().sorted(Comparator.comparing(Node::getX)).collect(Collectors.toList());
+
+        int ans = 0;
+        int valSum = 0;
+        int start = 0;
+
+        for (int i=0; i<2*N; i++) {
+            int x = nodes.get(i).x;
+            int v = nodes.get(i).v;
+            int index = nodes.get(i).index;
+            if (valSum == 0) {
+                start = x;
+            }
+            valSum += v;
+
+            if (valSum == 0) {
+                ans = Math.max(ans, x - start);
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
