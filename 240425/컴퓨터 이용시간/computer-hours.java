f376771fb6e5926e392e8078daa11b65c1114909
kochungcheon <g2c1010@naver.com>
2024-04-25 13:34:45 +0900

    Title: 컴퓨터 이용시간, Time: 1663ms, Memory: 41MB, Status: Passed - CodeTree

---
+++ 240425/컴퓨터 이용시간/computer-hours.java
@@ -0,0 +1,69 @@
+import java.util.*;
+import java.util.stream.*;
+import java.io.*;
+
+public class Main {
+    static class Node {
+        int time;
+        int type;
+        int index;
+
+        public Node(int time, int type, int index) {
+            this.time = time;
+            this.type = type;
+            this.index = index;
+        }
+
+        public int getTime() {
+            return this.time;
+        }
+
+        public int getType() {
+            return this.type;
+        }
+    }
+
+    static List<Node> nodes = new ArrayList<>();
+    static PriorityQueue<Integer> availableComputers = new PriorityQueue<>();
+    static int[] ans;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        ans = new int[N];
+
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            nodes.add(new Node(start, 1, i));
+            nodes.add(new Node(end, -1, i));
+        }
+
+        for (int i = 1; i <= N; i++) {
+            availableComputers.add(i);
+        }
+
+        nodes = nodes.stream()
+                     .sorted(Comparator.comparingInt(Node::getTime)
+                                       .thenComparingInt(Node::getType))
+                     .collect(Collectors.toList());
+
+        for (Node node : nodes) {
+            if (node.type == 1) { 
+                int computer = availableComputers.poll();
+                ans[node.index] = computer;
+            } else { 
+                availableComputers.add(ans[node.index]); 
+            }
+        }
+
+        // Print results
+        for (int i = 0; i < N; i++) {
+            System.out.print(ans[i] + " ");
+        }
+    }
+}
