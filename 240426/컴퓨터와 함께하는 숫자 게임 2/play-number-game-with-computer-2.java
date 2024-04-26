897a28d360e37a2d2c0042e9e03fc92bcbedbb19
kochungcheon <g2c1010@naver.com>
2024-04-26 16:16:53 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240426/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+public class Main {
+    static long m; 
+    static int a, b;
+    static int find(int target) {
+        int cnt = 0;
+        long s = 1; long e = m;
+        
+        while (e >= s) {
+            cnt++;
+            long mid = (s + e) / 2;
+            if (mid == target) {
+                return cnt;
+            }
+            if (mid > target) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return cnt;
+    }
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        m = Long.parseLong(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int ansMax = -1;
+        int ansMin = 100;
+        for (int i=a; i<=b; i++) {
+            int cnt = find(i);
+            ansMin = Math.min(ansMin, cnt);
+            ansMax = Math.max(ansMax, cnt);
+        }
+        System.out.println(ansMin + " " + ansMax);
+    }
+}
