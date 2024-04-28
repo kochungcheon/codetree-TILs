9385e9e13bf043b3863ab5371b5267fc98419920
kochungcheon <g2c1010@naver.com>
2024-04-28 19:12:50 +0900

    Title: 삼 오 무, Time: 3058ms, Memory: 8MB, Status: Time Limit Exceed - CodeTree

---
+++ 240428/삼 오 무/three-five-moo.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int ans = Integer.MAX_VALUE;
+    static boolean check(int target) {
+        return N <= target - target / 3 - target / 5 + target / 15;
+    }
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        int s = 1;
+        int e = Integer.MAX_VALUE;
+        
+        while (e >= s) {
+            int mid = (s + e) / 2;
+
+            if (check(mid)) {
+                e = mid - 1;
+                ans = Math.min(ans, mid);
+            } else {
+                s = mid + 1;
+            }
+        }
+
+        System.out.print(ans);
+    }
+}
