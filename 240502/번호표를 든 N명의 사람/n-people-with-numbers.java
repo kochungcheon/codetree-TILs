eacdc9ff2917fe699fd99a24c4366b099bd68ea4
kochungcheon <g2c1010@naver.com>
2024-05-02 15:11:33 +0900

    Title: 번호표를 든 N명의 사람, Time: 2326ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 240502/번호표를 든 N명의 사람/n-people-with-numbers.java
+++ 240502/번호표를 든 N명의 사람/n-people-with-numbers.java
@@ -39,18 +39,28 @@ public class Main {
         for (int i=0; i<n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-
-        int s = 1;
-        int e = n;
-        int ans = 10000;
-        while (e >= s) {
-            int mid = (s + e) / 2;
-            if (check(mid)) {
-                e = mid - 1;
-                ans = Math.min(ans, mid);
+        int ans = 0;
+        for (int k=n; k>=1; k--) {
+            int time = 0;
+            int mt = 0;
+            PriorityQueue<Integer> pq = new PriorityQueue<>();
+            for (int i=0; i<k; i++) {
+                pq.add(time + arr[i]);
+                mt = Math.max(time + arr[i], mt);
             }
+            // 남은 친구들 처리
+            for (int i=k; i<n; i++) {
+                int now = pq.poll();
+                time = now;
+                pq.add(time + arr[i]);
+                mt = Math.max(time + arr[i], mt);
+            }
+
+            if (mt <= tMax) {
+                ans = k;
+            } 
             else {
-                s = mid + 1;
+                break;
             }
         }
 
