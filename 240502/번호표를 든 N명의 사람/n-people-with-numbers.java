2799497535a73d1a33afb2245295868ed22c4e25
kochungcheon <g2c1010@naver.com>
2024-05-02 15:14:37 +0900

    Title: 번호표를 든 N명의 사람, Time: 3052ms, Memory: 120MB, Status: Time Limit Exceed - CodeTree

--- 240502/번호표를 든 N명의 사람/n-people-with-numbers.java
+++ 240502/번호표를 든 N명의 사람/n-people-with-numbers.java
@@ -5,26 +5,7 @@ import java.io.*;
 public class Main {
     static int n, tMax;
     static int[] arr;
-
-    static boolean check(int target) {
-        int time = 0;
-        int mt = 0;
-        // target 만큼 큐에 넣기 time + di 를 기준으로 넣자
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int i=0; i<target; i++) {
-            pq.add(time + arr[i]);
-            mt = Math.max(time + arr[i], mt);
-        }
-        // 남은 친구들 처리
-        for (int i=target; i<n; i++) {
-            int now = pq.poll();
-            time = now;
-            pq.add(time + arr[i]);
-            mt = Math.max(time + arr[i], mt);
-        }
-
-        return mt <= tMax;
-    }
+    static PriorityQueue<Integer> pq;
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -43,7 +24,7 @@ public class Main {
         for (int k=n; k>=1; k--) {
             int time = 0;
             int mt = 0;
-            PriorityQueue<Integer> pq = new PriorityQueue<>();
+            pq = new PriorityQueue<>();
             for (int i=0; i<k; i++) {
                 pq.add(time + arr[i]);
                 mt = Math.max(time + arr[i], mt);
