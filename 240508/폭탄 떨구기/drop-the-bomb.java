148acd56bac676e583d65f23f5805a8ce64261bc
kochungcheon <g2c1010@naver.com>
2024-05-08 17:47:21 +0900

    Title: 폭탄 떨구기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240508/폭탄 떨구기/drop-the-bomb.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.util.stream.*;
+import java.io.*;
+public class Main {
+    static int N, K;
+    static int[] arr;
+    static boolean check(int target) {
+        int cnt = 1;
+        int compare = arr[0];
+        for (int a : arr) {
+            if (a - compare > target) {
+                cnt++;
+                compare = a;
+            }
+        }
+        return K >= cnt;
+    }
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // 점 개수
+        N = Integer.parseInt(st.nextToken());
+        // 폭탄 개수
+        K = Integer.parseInt(st.nextToken());
+
+        for (int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        int ans = 1000000000;
+
+        int s = 0;
+        int e = 1000000000;
+
+        while (e >= s) {
+            int mid = s + (e - s) / 2;
+            if (check(mid)) {
+                ans = Math.min(ans, mid);
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        System.out.println(ans);
+    }
+}
